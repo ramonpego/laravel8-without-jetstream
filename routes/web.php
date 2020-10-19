20b9cfc89865b9d204b2c7aee8b4d60a4503772b
Ramon Pêgo <ramon.pego.stl@gmail.com>
2020-10-19 17:57:35 -0300

    traduções

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('home', fn() => 'Authenticated');
+Route::get('home', fn() => 'Authenticated')->middleware('auth');
 
 
